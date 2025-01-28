3343cc3104098a54fc347ebaa73565a5864dab61
MariusBbd1 <marius.barbaud@etu.univ-cotedazur.fr>
2025-01-28 14:37:06 +0100

    function extend lending period, An employee can extend the lending period by 2 weeks once

---
+++ functions/Extend_lending_period.sql
@@ -0,0 +1,15 @@
+-- You enter the function with the id of the lending you want to extend the period of. --
+-- The function will update the end_date of the lending by adding 14 days to it --
+-- if the lending is not returned, the end_date is greater than the current date, --
+-- and the difference between the end_date and begin_date is <= 31 days (aka the lending hasn't been extended) --
+CREATE OR REPLACE FUNCTION EXTEND_LENDING_PERIOD(lending_id INTEGER)
+	RETURNS VOID as $$
+	BEGIN
+		UPDATE lendings
+		SET end_date = end_date + INTERVAL '14 days'
+        WHERE returned = FALSE
+            AND end_date > CURRENT_DATE
+            AND end_date-begin_date <= 31
+            AND id_lending = lending_id;
+	END;
+	$$ LANGUAGE plpgsql;
