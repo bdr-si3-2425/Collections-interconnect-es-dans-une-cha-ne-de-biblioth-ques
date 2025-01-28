f5625b55c07ae46f8f44ac0ba07ffaf9621fc909
MariusBbd1 <marius.barbaud@etu.univ-cotedazur.fr>
2025-01-28 14:13:08 +0100

    function 'violate' that updates the delays for unreturned lendings

---
+++ functions/update_lending_delays.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE FUNCTION UPDATE_LENDING_DELAYS()
+	RETURNS VOID as $$
+	BEGIN
+		UPDATE lendings
+		SET delays = CURRENT_DATE - end_date
+		WHERE returned = FALSE
+			AND end_date < CURRENT_DATE;
+	END;
+	$$ LANGUAGE plpgsql;
+	
+	
