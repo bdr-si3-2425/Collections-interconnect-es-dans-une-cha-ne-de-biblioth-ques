ef21d53b5de4d1e838dfa182ba84bb974f45d4d6
MariusBbd1 <marius.barbaud@etu.univ-cotedazur.fr>
2025-01-29 11:27:13 +0100

    trigger return lending (missing function not added last commit)

---
+++ functions/Return_lendings_books.sql
@@ -0,0 +1,19 @@
+--Function called by the trigger Lending_returned_trigger--
+--updates the quantity of books in holdings when a lending is returned--
+--Calls add_book function to increase the quantity of books in holdings--
+
+CREATE OR REPLACE FUNCTION return_lendings_books()
+RETURNS TRIGGER AS $$
+DECLARE rec RECORD;
+BEGIN
+    IF OLD.returned = FALSE AND NEW.returned = TRUE THEN
+        FOR rec IN
+        SELECT * FROM is_lended
+        WHERE id_lending = NEW.id_lending
+    LOOP
+        PERFORM add_book(rec.isbn, rec.id_library);
+    END LOOP;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
