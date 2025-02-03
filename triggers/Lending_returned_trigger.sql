4214102ef95fdcecc5b85ff3d9946aa7997ca1a7
MariusBbd1 <marius.barbaud@etu.univ-cotedazur.fr>
2025-02-03 22:31:37 +0100

    added some comments to previously uncommented functions

--- triggers/Lending_returned_trigger.sql
+++ triggers/Lending_returned_trigger.sql
@@ -1,3 +1,5 @@
+-- This trigger triggers when the 'returned' column of a lending switches from false to true --
+-- This trigger calls the return_lendings_books function that returns the books that were lended to the lending library --
 CREATE TRIGGER Lending_returned_trigger
     AFTER UPDATE
     ON LENDINGS FOR EACH ROW
